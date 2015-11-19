b47204edb6340dcbb1a597ad31503d6182f25d98
Juan Pablo Marzetti <jmarzetti@groupon.com>
2015-11-19 14:41:32 -0800

    Initial Commit

---
+++ Tinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tinder
+//
+//  Created by Juan Pablo Marzetti on 11/19/15.
+//  Copyright © 2015 Juan Pablo Marzetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tinder
+//
+//  Created by Juan Pablo Marzetti on 11/19/15.
+//  Copyright © 2015 Juan Pablo Marzetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
